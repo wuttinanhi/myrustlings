2aa2c5ff18d7216e77a0142c8112f06129511444
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2024-09-14 09:28:26 +0700

    complete 04_primitive_types

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -9,8 +9,22 @@ mod tests {
         let a = [1, 2, 3, 4, 5];
 
         // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
-        // let nice_slice = ???
+        let nice_slice = &a[1..4];
 
         assert_eq!([2, 3, 4], nice_slice);
+
+        // TRY SOMETHING
+        // let mut a_cloned = a.clone();
+        // dbg!(a_cloned);
+
+        // let edit_slice = &mut a_cloned[1..4];
+
+        // dbg!(&edit_slice);
+
+        // edit_slice[0] = 0;
+        // edit_slice[1] = 0;
+        // edit_slice[2] = 0;
+
+        // dbg!(&edit_slice);
     }
 }

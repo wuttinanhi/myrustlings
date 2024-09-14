d72a8a05de9f3180505e96a13d450ed5d6a55166
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2024-09-14 09:42:02 +0700

    complete 06_move_semantics

--- exercises/06_move_semantics/move_semantics3.rs
+++ exercises/06_move_semantics/move_semantics3.rs
@@ -1,5 +1,5 @@
 // TODO: Fix the compiler error in the function without adding any new line.
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
     vec.push(88);
 
     vec
@@ -15,7 +15,7 @@ mod tests {
 
     #[test]
     fn move_semantics3() {
-        let vec0 = vec![22, 44, 66];
+        let mut vec0 = vec![22, 44, 66];
         let vec1 = fill_vec(vec0);
         assert_eq!(vec1, [22, 44, 66, 88]);
     }

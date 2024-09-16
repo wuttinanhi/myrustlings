961f095316e25a57ee3e6e5b3bbe6688f22d575a
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2024-09-16 15:28:07 +0700

    complete 11_hashmaps

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -27,10 +27,15 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         let team_1_score: u8 = split_iterator.next().unwrap().parse().unwrap();
         let team_2_score: u8 = split_iterator.next().unwrap().parse().unwrap();
 
-        // TODO: Populate the scores table with the extracted details.
-        // Keep in mind that goals scored by team 1 will be the number of goals
-        // conceded by team 2. Similarly, goals scored by team 2 will be the
-        // number of goals conceded by team 1.
+        // Update team 1's scores
+        let team_1_entry = scores.entry(team_1_name).or_insert(TeamScores::default());
+        team_1_entry.goals_scored += team_1_score;
+        team_1_entry.goals_conceded += team_2_score;
+
+        // Update team 2's scores
+        let team_2_entry = scores.entry(team_2_name).or_insert(TeamScores::default());
+        team_2_entry.goals_scored += team_2_score;
+        team_2_entry.goals_conceded += team_1_score;
     }
 
     scores
